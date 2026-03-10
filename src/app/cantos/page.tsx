5f6cae6b5bdb593187d8f5a81469e5fde2154d5c
Laura Carbajal <laura.carbajal@itsn.edu.mx>
2026-03-10 17:53:57 -0600

    Efecto responsivo 2 en Cantos

--- src/app/cantos/page.tsx
+++ src/app/cantos/page.tsx
@@ -60,7 +60,7 @@ filter === "Todos" || song.category?.toLowerCase() === filter.toLowerCase();
   Cantos del Ministerio
 </h1>
 
-<div className="relative w-64">
+<div className="relative w-full sm:w-64">
 
 <svg
   xmlns="http://www.w3.org/2000/svg"
@@ -83,10 +83,12 @@ filter === "Todos" || song.category?.toLowerCase() === filter.toLowerCase();
   value={search}
   onChange={(e) => setSearch(e.target.value)}
   className="w-full pl-10 pr-4 py-2 rounded-full
-  border border-purple-300
-  bg-white
-  shadow-sm
-  focus:outline-none focus:ring-2 focus:ring-purple-400"
+border border-purple-300
+bg-white
+text-gray-800
+placeholder-gray-400
+shadow-sm
+focus:outline-none focus:ring-2 focus:ring-purple-400"
 />
 
 </div>
